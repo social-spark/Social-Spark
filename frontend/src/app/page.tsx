0ddfa576d240e3e00df1db94c57a39180edbb06f
Isaac Jardines <ijardines@cnm.edu>
2024-05-14 16:36:54 -0600

    clean up aisle app page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,6 @@
 'use client'
 import React from "react";
-import {main} from "@popperjs/core";
 import {Navigation} from "@/app/components/Navigation";
-import {Login} from "@/app/Login";
-import {Nav} from "@/app/components/Nav";
-
 
 
 export default function Home() {
