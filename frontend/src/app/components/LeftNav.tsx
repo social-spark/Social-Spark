a3e90fa094d77e546cfa1a4c0d51317b8934fcdd
Monica Mendez <smendez22@cnm.edu>
2024-05-20 16:21:09 -0600

    centered leftnav

--- frontend/src/app/components/LeftNav.tsx
+++ frontend/src/app/components/LeftNav.tsx
@@ -2,8 +2,6 @@
 import trending from "@/app/images/trending.png"
 import friends from "@/app/images/friends.png"
 import {Navbar, Sidebar} from "flowbite-react";
-import { BiBuoy } from "react-icons/bi";
-import { HiArrowSmRight, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
 import Image from "next/image";
 
 export function LeftNav() {
@@ -11,7 +9,7 @@ export function LeftNav() {
         <Sidebar aria-label="Sidebar with content separator example">
 
             <Sidebar.Items>
-                <Sidebar.ItemGroup >
+                <Sidebar.ItemGroup className="text-center">
                     <Sidebar.Item href="#"><Image className="size-7 inline-flex tracking-normal" src={friends} alt="friends icon"/>
                         Prompt Friends
                     </Sidebar.Item>
@@ -26,8 +24,9 @@ export function LeftNav() {
                         <div className="underline decoration-blue-400 text-center">username</div>
                     </Sidebar.Item>
                 </Sidebar.ItemGroup>
-                <Sidebar.ItemGroup>
-                    <Sidebar.Item href="#"><Image className="size-7" src={trending} alt="trending prompts icon"/>
+                <Sidebar.ItemGroup className="text-center">
+                    <Sidebar.Item href="#"><p className="inline-flex">Trending</p>
+                        <Image className="size-7 float-right" src={trending} alt="trending prompts icon"/>
                     </Sidebar.Item>
                     <Sidebar.Item href="#">
                         #Art&Culture
