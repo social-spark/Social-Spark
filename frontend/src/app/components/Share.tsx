1fee7bf1d7484169c23957a97c994ed607b749e1
Jaime Garcia <jgarcia1138@cnm.edu>
2024-05-20 16:24:59 -0600

    added the like componant as well as the post

---
+++ frontend/src/app/components/Share.tsx
@@ -0,0 +1,29 @@
+import shareicon from "../images/shareicon.png"
+import Image from "next/image";
+
+export function Share() {
+    return (
+        <div className="relative">
+        <button type="button" className=" size-6 transform rotate-90 absolute top-[1.5rem] right-[0.5rem] ">
+            <Image src={shareicon} alt="share icon"/>
+        </button>
+        </div>
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ frontend/src/app/profile/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import {Navigation} from "@/app/components/Navigation";
+import {Button, Checkbox, Label, TextInput} from "flowbite-react";
+import React from "react";
+import {Post} from "@/app/components/Post";
+
+export default function ProfileHeader () {
+    return (
+        <main>
+            <Navigation/>
+
+            <Post/>
+
+
+        </main>
+    )
+}
