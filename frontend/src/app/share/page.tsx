bcc22c5d4628ae49b81b135904b29450ba0a9c25
Isaac Jardines <ijardines@cnm.edu>
2024-05-23 20:17:58 -0600

    done with layout need to center card

--- frontend/src/app/share/page.tsx
+++ frontend/src/app/share/page.tsx
@@ -1,15 +1,28 @@
+'use client'
 import profile from '@/app/images/profile.png'
 import Image from "next/image";
+import {Post} from "@/app/components/Post";
+
 
 
 export default function Share() {
     return (
-        <header>
-            <div className="box-border h-full w-96 p-4 border-4">
-                <Image className="w-20 h-20 rounded-full"  src={profile} alt="profile picture"/>
-                <p className="">User Name</p>
-                <p className="">Full Name</p>
+        <body>
+        <div className="box-border sm:w-1/4 p-4 border-4 ">
+            <div className="flex items-center">
+                <Image className="w-20 h-20 rounded-full" src={profile} alt="profile picture"/>
+                <div className="ml-4">
+                    <p className="font-bold">User Name</p>
+                    <p className="text-sm text-gray-600">Full Name</p>
+                </div>
             </div>
-        </header>
+            <Post/>
+            <div className="mt-6 flex items-center justify-end">
+                <button type="submit"
+                        className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Share
+                </button>
+            </div>
+        </div>
+        </body>
     );
 }
