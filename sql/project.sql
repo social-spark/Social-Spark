4d5fce2e9e82a58d96ad6a37200e876ab104a79f
Monica Mendez <smendez22@cnm.edu>
2024-05-29 11:54:47 -0600

    added sql file

---
+++ sql/project.sql
@@ -0,0 +1,74 @@
+
+
+
+CREATE TABLE profile (
+ profile_id uuid not null,
+ profile_activation_token char(32),
+ profile_bio varchar(200),
+ profile_date_created timestamp with time zone not null,
+ profile_email varchar(80) not null unique,
+ profile_full_name varchar(20) not null,
+ profile_image varchar,
+ profile_hash char(97) not null,
+ profile_user_name varchar(15) not null unique,
+ Primary Key (profile_id)
+);
+
+
+CREATE TABLE prompt(
+prompt_id uuid not null,
+prompt_body varchar(128) not null,
+prompt_category varchar(32) not null,
+Primary Key (prompt_id)
+);
+
+CREATE TABLE post (
+post_id uuid not null,
+post_profile_id uuid not null,
+post_prompt_id uuid not null,
+post_body varchar(200),
+post_date timestamp with time zone not null,
+post_image varchar,
+Primary Key (post_id),
+Foreign Key (post_profile_id) references profile(profile_id),
+Foreign Key (post_prompt_id) references prompt(prompt_id)
+);
+Create Index on post(post_profile_id);
+Create Index on post(post_prompt_id);
+
+CREATE TABLE follow(
+following_profile_id uuid not null,
+followed_profile_id uuid not null,
+follow_date_created timestamp with time zone not null,
+Primary Key (following_profile_id, followed_profile_id),
+Foreign Key (following_profile_id) references profile(profile_id),
+Foreign Key (followed_profile_id) references profile(profile_id)
+);
+Create Index on follow(following_profile_id);
+Create Index on follow(followed_profile_id);
+
+CREATE TABLE  "like" (
+like_post_id uuid not null,
+like_profile_id uuid not null,
+like_date timestamp with time zone not null,
+Primary Key (like_post_id, like_profile_id),
+Foreign Key (like_post_id) references post(post_id),
+Foreign Key (like_profile_id) references profile(profile_id)
+);
+Create Index on "like"(like_post_id);
+Create Index on "like"(like_profile_id);
+
+
+CREATE TABLE notification (
+notification_like_post_id uuid not null,
+notification_profile_id uuid not null,
+notification_like_profile_id uuid not null,
+notification_date timestamp with time zone not null,
+notification_read boolean not null,
+Primary Key (notification_like_post_id, notification_profile_id, notification_like_profile_id),
+Foreign Key (notification_profile_id) references profile(profile_id),
+Foreign Key (notification_like_profile_id, notification_like_post_id) references "like"(like_profile_id, like_post_id)
+);
+Create Index on notification(notification_profile_id);
+Create Index on notification(notification_like_profile_id, notification_like_post_id);
+
