b9dab2990ab3a0e1a4f5eb85b22fdaa952245713
Jaime Garcia <jgarcia1138@cnm.edu>
2024-05-30 14:08:05 -0600

    add mail gun information

--- backend/src/apis/sign-up/sign-up.controller.ts
+++ backend/src/apis/sign-up/sign-up.controller.ts
@@ -4,6 +4,8 @@ import {setActivationToken, setHash} from "../../utils/auth.utils";
 import {Request, Response} from "express"
 import {insertProfile, PrivateProfile} from "../profile/profile.model";
 import {Status} from "../../utils/interfaces/Status";
+import formData from 'form-data'
+import Mailgun from 'mailgun.js'
 
 export async function signupProfileController (request: Request, response: Response): Promise<Response | undefined>
 {
@@ -38,6 +40,26 @@ export async function signupProfileController (request: Request, response: Respo
         await insertProfile(profile)
 
         //send activation email
+        // create a new mailgun client with the mailgun api key
+        const mailgun: Mailgun = new Mailgun(formData)
+        const mailgunClient = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY as string })
+        // create a basePath variable containing the scheme, host, port, and base path
+        const basePath: string = `${request.protocol}://${request.hostname}:8080${request.originalUrl}activation/${profileActivationToken}`
+
+        // create a message for the activation email body
+        const message = `<h2>Welcome to Prompt-Me.</h2>
+        <p>In order to start posting you must confirm your account.</p>
+        <p><a href="${basePath}">${basePath}</a></p>`
+
+        // create a mailgun message object
+        const mailgunMessage = {
+            from: `Mailgun Sandbox <postmaster@${process.env.MAILGUN_DOMAIN as string}>`,
+            to: profileEmail,
+            subject: 'Activate your Prompt-Me Account!',
+            html: message
+        }
+        // send the email
+        await mailgunClient.messages.create(process.env.MAILGUN_DOMAIN as string, mailgunMessage)
 
 
         // create a status object to send back to the client
