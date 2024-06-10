3a311aa818342882b0c440dd75771502fbddd58a
Jaime Garcia <jgarcia1138@cnm.edu>
2024-06-10 16:28:51 -0600

    changed the layout of the profile page and added a background

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import {Flowbite, theme, ThemeModeScript} from "flowbite-react";
 
 
 export const metadata: Metadata = {
-    title: 'Title Goes Here',
-    description: 'description goes here',
+    title: 'Prompt Me',
+    description: '',
 }
 
 type RootLayoutProps = {
@@ -14,8 +15,19 @@ type RootLayoutProps = {
 export default function RootLayout(props : RootLayoutProps) {
     const { children } = props
     return (
-        <html  lang="en" suppressHydrationWarning>
-        <body>{children}</body>
+        <html>
+        <head>
+
+
+            <link href="/frontend/src/app/images/promptMeLogo.png" rel="icon" type="image/x-icon"/>
+            <ThemeModeScript/><title>Prompt Me</title>
+        </head>
+        <body className={("min-h-dvh bg-[#50646F] font-sans antialiasing text-stone-800 dark:bg-gray-800 dark:text-slate-200")} >
+
+        <Flowbite theme={{theme: theme}}>
+            {children}
+        </Flowbite>
+        </body>
         </html>
     )
 }
--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -10,54 +10,99 @@ import setting from "@/app/images/settingsicon.png"
 import Image from "next/image";
 import {PromptBox} from "@/app/components/PromptBox";
 
-
-
-export default function ProfileHeader () {
+export default function ProfileHeader() {
     return (
-        <main className="container mx-auto rounded-lg">
-            <Navigation/>
-        <PromptBox/>
+        <main className="container mx-auto">
+            <Navigation />
 
-        <section className="grid grid-cols-3 md:grid-cols-3">
+            <section className="flex flex-row md:flex-row pt-6 space-y-6 md:space-y-0 md:space-x-6">
+                <PromptBox />
 
-
-            <div className="grid grid-cols-subgrid gap-4 col-span-3">
-                <section className="container border border-slate-950 col-start-1 col-span-3">
-
-                    <div className="grid md:grid-cols-4 grid-cols-3">
-                        <Image className="object-scale-down h-36 w-36 p-5" src={profile} alt="Sunset in the mountains"/>
-
-                        <div className="col-start-2">
-                            <p className="py-1">@UserName</p>
-                            <p className="py-1">Full Name</p>
-                            <TextInput id="Bio" type="Bio" placeholder="Bio" className="py-3"/>
-                        </div>
-
-                        <div
-                            className="md:col-start-4 col-start-3 md:flex md:flex-row flex flex-col flex-wrap place-content-evenlynp">
-                            <button type="button"
-                                    className="self-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Follow
-                            </button>
-                            <button className="w-30 mx-auto">
-                                <Image className="" width={30}
-                                       height={30} src={setting} alt="setting icon"/>
-                            </button>
-                        </div>
-                    </div>
-                </section>
-
-                <div className="hidden lg:block">
-                    <LeftNav/>
+                <div className="hidden lg:inline-block">
+                    <LeftNav />
                 </div>
 
+                <div className="flex-1">
+                    <section className="grid grid-cols-2 md:grid md:grid-cols-3 border border-slate-950 rounded-lg bg-white p-6 md:space-x-6"
+                        id="profile-header">
+                        <Image className="object-scale-down h-36 w-36 row-span-2 " src={profile} alt="Profile picture"/>
 
-                <div className="lg:col-start-2 lg:col-span-4 sm:pl-11 col-start-1 col-span-4">
+                        <div className="col-start-2  space-y-2 md:space-y-0">
+                            <p className="py-1 text-2xl font-bold">@UserName</p>
+                            <p className="py-1 text-xl">Full Name</p>
+                        </div>
+
+                        <div className="md:col-start-3 flex items-center space-y-2 md:space-y-0 md:space-x-2 space-x-4">
+                            <button type="button"
+                                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-2 focus:outline-none">
+                                Follow
+                            </button>
+                            <button className="flex items-center justify-center">
+                                <Image width={40} height={40} src={setting} alt="Setting icon"/>
+                            </button>
+                        </div>
+
+                        <p className="col-span-2 md:col-start-2 md:col-span-2 text-l pt-5">Lorem ipsum dolor sit amet,
+                            consectetur adipisicing elit.
+                            Voluptatibus
+                            quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+
+                    </section>
                     <Posts/>
                 </div>
-            </div>
-        </section>
 
+            </section>
 
         </main>
-    )
+    );
 }
+
+// export default function ProfileHeader () {
+//     return (
+//         <main className="container mx-auto rounded-lg">
+//             <Navigation/>
+//         <PromptBox/>
+//
+//         <section className="grid grid-cols-3 md:grid-cols-3">
+//
+//
+//             <div className="grid grid-cols-subgrid gap-4 col-span-3 pt-6">
+//                 <section className="container border border-slate-950 rounded-lg col-start-1 col-span-3 bg-white">
+//
+//                     <div className="grid md:grid-cols-4 grid-cols-3 ">
+//                         <Image className="object-scale-down h-36 w-36 p-5" src={profile} alt="Sunset in the mountains"/>
+//
+//                         <div className="col-start-2">
+//                             <p className="py-1">@UserName</p>
+//                             <p className="py-1">Full Name</p>
+//                             <TextInput id="Bio" type="Bio" placeholder="Bio" className="py-3"/>
+//                         </div>
+//
+//                         <div
+//                             className="md:col-start-4 col-start-3 md:flex md:flex-row flex flex-col flex-wrap place-content-evenly">
+//                             <button type="button"
+//                                     className="self-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Follow
+//                             </button>
+//                             <button className="w-30 mx-auto">
+//                                 <Image className="" width={30}
+//                                        height={30} src={setting} alt="setting icon"/>
+//                             </button>
+//                         </div>
+//                     </div>
+//                 </section>
+//
+//                 <div className="hidden lg:block">
+//                     <LeftNav/>
+//                 </div>
+//
+//
+//                 <div className="lg:col-start-2 lg:col-span-4 sm:pl-11 col-start-1 col-span-4">
+//                     <Posts/>
+//                 </div>
+//             </div>
+//         </section>
+//
+//
+//         </main>
+//     )
+// }
