7bf9bdcdd37abc9320a8fe36a9866db76536f272
Isaac Jardines <ijardines@cnm.edu>
2024-06-12 11:11:26 -0600

    messing with colors

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import {Flowbite, Sidebar, theme, ThemeModeScript} from "flowbite-react";
+import {Flowbite, Sidebar,  ThemeModeScript} from "flowbite-react";
 import {Nav} from "@/app/components/Nav";
+import {LeftNav} from "@/app/components/LeftNav";
 
 
 export const metadata: Metadata = {
@@ -18,13 +19,11 @@ export default function RootLayout(props : RootLayoutProps) {
     return (
         <html>
         <head>
-
-
             <link href="/frontend/src/app/images/promptMeLogo.png" rel="icon" type="image/x-icon"/>
             <ThemeModeScript/><title>Prompt Me</title>
         </head>
         <body className={("min-h-dvh bg-[#50646F] font-sans antialiasing text-stone-800 dark:bg-gray-800 dark:text-slate-200")} >
-        <Flowbite theme={{theme: theme}}>
+        <Flowbite>
             {children}
         </Flowbite>
         </body>
